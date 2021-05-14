e8ebe5d0c126159c6360bafab381292edd3952a2
sonokr <ryotakusunoki@hotmail.com>
2021-05-14 16:43:54 +0900

    formatted by black

--- Code/TrackNet_Three_Frames_Input/Models/__init__.py
+++ Code/TrackNet_Three_Frames_Input/Models/__init__.py
@@ -1,2 +1,1 @@
-import TrackNet
-
+from . import TrackNet
--- Code/TrackNet_Three_Frames_Input/train.py
+++ Code/TrackNet_Three_Frames_Input/train.py
@@ -6,17 +6,17 @@ from keras.utils import plot_model
 import LoadBatches
 import Models
 
-#parse parameters
+# parse parameters
 parser = argparse.ArgumentParser()
-parser.add_argument("--save_weights_path", type = str  )
-parser.add_argument("--training_images_name", type = str  )
-parser.add_argument("--n_classes", type=int )
-parser.add_argument("--input_height", type=int , default = 360  )
-parser.add_argument("--input_width", type=int , default = 640 )
-parser.add_argument("--epochs", type = int, default = 1000 )
-parser.add_argument("--batch_size", type = int, default = 2 )
-parser.add_argument("--load_weights", type = str , default = "-1")
-parser.add_argument("--step_per_epochs", type = int, default = 200 )
+parser.add_argument("--save_weights_path", type=str)
+parser.add_argument("--training_images_name", type=str)
+parser.add_argument("--n_classes", type=int)
+parser.add_argument("--input_height", type=int, default=360)
+parser.add_argument("--input_width", type=int, default=640)
+parser.add_argument("--epochs", type=int, default=1000)
+parser.add_argument("--batch_size", type=int, default=2)
+parser.add_argument("--load_weights", type=str, default="-1")
+parser.add_argument("--step_per_epochs", type=int, default=200)
 
 args = parser.parse_args()
 training_images_name = args.training_images_name
@@ -30,37 +30,45 @@ load_weights = args.load_weights
 step_per_epochs = args.step_per_epochs
 optimizer_name = optimizers.Adadelta(lr=1.0)
 
-#load TrackNet model
+# load TrackNet model
 modelTN = Models.TrackNet.TrackNet
-m = modelTN( n_classes , input_height=input_height, input_width=input_width   )
-m.compile(loss='categorical_crossentropy', optimizer= optimizer_name, metrics=['accuracy'])
+m = modelTN(n_classes, input_height=input_height, input_width=input_width)
+m.compile(loss="categorical_crossentropy", optimizer=optimizer_name, metrics=["accuracy"])
 
-#check if need to retrain the model weights
+# check if need to retrain the model weights
 if load_weights != "-1":
-	m.load_weights("weights/model." + load_weights)
+    m.load_weights("weights/model." + load_weights)
 
-#show TrackNet details, save it as TrackNet.png
-plot_model( m , show_shapes=True , to_file='TrackNet.png')
+# show TrackNet details, save it as TrackNet.png
+plot_model(m, show_shapes=True, to_file="TrackNet.png")
 
-#get TrackNet output height and width
+# get TrackNet output height and width
 model_output_height = m.outputHeight
 model_output_width = m.outputWidth
 
-#creat input data and output data
-Generator  = LoadBatches.InputOutputGenerator( training_images_name,  train_batch_size,  n_classes , input_height , input_width , model_output_height , model_output_width)
+# creat input data and output data
+Generator = LoadBatches.InputOutputGenerator(
+    training_images_name,
+    train_batch_size,
+    n_classes,
+    input_height,
+    input_width,
+    model_output_height,
+    model_output_width,
+)
 
 
-#start to train the model, and save weights until finish
-'''
+# start to train the model, and save weights until finish
+"""
 m.fit_generator( Generator, step_per_epochs, epochs )
 m.save_weights( save_weights_path + ".0" )
 
-'''
+"""
 
-#start to train the model, and save weights per 50 epochs
+# start to train the model, and save weights per 50 epochs
 
-for ep in range(1, epochs+1 ):
-	print("Epoch :" + str(ep) + "/" + str(epochs))
-	m.fit_generator(Generator, step_per_epochs)
-	if ep % 50 == 0:
-		m.save_weights(save_weights_path + ".0")
+for ep in range(1, epochs + 1):
+    print("Epoch :" + str(ep) + "/" + str(epochs))
+    m.fit_generator(Generator, step_per_epochs)
+    if ep % 50 == 0:
+        m.save_weights(save_weights_path + ".0")
